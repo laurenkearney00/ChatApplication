514dc19aa13bf8268277f72c4ffe60b09eaad7f8
laurenkearney00 <c18330401@mytudublin.ie>
2022-03-25 11:20:40 +0000

    Added toast message

--- app/src/main/java/com/example/chatapplication/Message.java
+++ app/src/main/java/com/example/chatapplication/Message.java
@@ -3,6 +3,7 @@ package com.example.chatapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Message extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class Message extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message2);
+        Toast.makeText(Message.this, "All fileds are required", Toast.LENGTH_SHORT).show();
+
     }
 }
